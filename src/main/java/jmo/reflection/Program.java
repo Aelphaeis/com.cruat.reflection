c9c12383e39721a9c18f68a62d42b96443a17437
Joseph Morain <morain@echoworx.com>
2018-04-13 10:53:51 -0400

    EntryPoint defined.

---
+++ src/main/java/jmo/reflection/Program.java
@@ -0,0 +1,9 @@
+package jmo.reflection;
+
+import org.apache.logging.log4j.LogManager;
+
+public class Program {
+	public static void main(String[] args) {
+		LogManager.getLogger().trace("Hello World!");
+	}
+}

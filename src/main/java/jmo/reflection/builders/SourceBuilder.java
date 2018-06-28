844a47ac0f2b1c4d561fa2e974fd74d0fed379b3
Joseph Morain <morain@echoworx.com>
2018-06-28 11:46:25 -0400

    clean up SourceBuilder.

--- src/main/java/jmo/reflection/builders/SourceBuilder.java
+++ src/main/java/jmo/reflection/builders/SourceBuilder.java
@@ -1,24 +1,15 @@
 package jmo.reflection.builders;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import static jmo.reflection.builders.SourceBuilderUtils.getTemplate;
 
 import jmo.reflection.core.JClass;
-import jmo.reflection.core.SourceTemplates;
 
 public class SourceBuilder {
-	private static final String TEMP_LOC = "src/main/resources/template.txt";
-	private static String getTemplate() {
-		try {
-			return new String(Files.readAllBytes(Paths.get(TEMP_LOC)));
-		} catch (IOException e) {
-			throw new IllegalStateException(e);
-		}
-	}
+	public static final String PACKAGE = "{{package}}";
+	public static final String TEMPLATE = getTemplate();
+
 
 	final JClass<?> cls;
-	String source;
 
 	public SourceBuilder() {
 		this(null);
@@ -28,24 +19,12 @@ public class SourceBuilder {
 	}
 
 	public String build() {
-		source = getTemplate();
-		return source;
-	}
-
-	String buildConstruct(JClass<?> type) {
-		if (type.isEnum()) {
-			return SourceTemplates.CLASS_MODIFIER.format("enum");
-		} else if (type.isInterface()) {
-			return SourceTemplates.CLASS_MODIFIER.format("interface");
-		} else if (type.isAnnotation()) {
-			return SourceTemplates.CLASS_MODIFIER.format("@interface");
-		} else {
-			return SourceTemplates.CLASS_MODIFIER.format("class");
-		}
+		return null;
 	}
 
 	@Override
 	public String toString() {
 		return build();
 	}
+
 }

1fc9aca5340df08ebddc63e5b2a8e9848cdba2e0
Joseph Morain <morain@echoworx.com>
2018-06-28 11:44:47 -0400

    added new method for building modifiers

--- src/main/java/jmo/reflection/builders/SourceBuilderUtils.java
+++ src/main/java/jmo/reflection/builders/SourceBuilderUtils.java
@@ -1,5 +1,7 @@
 package jmo.reflection.builders;
 
+import java.lang.reflect.Modifier;
+
 import jmo.reflection.core.JClass;
 import jmo.reflection.core.SourceTemplates;
 
@@ -16,6 +18,30 @@ final class SourceBuilderUtils {
 			return SourceTemplates.CLASS_MODIFIER.format("class");
 		}
 	}
+	
+	public static String buildClassModifiers(JClass<?> type) {
+		int mods = type.getModifiers();
+		StringBuilder builder = new StringBuilder();
+		if (Modifier.isPublic(mods)) {
+			builder.append(SourceTemplates.CLASS_MODIFIER.format("public"));
+		} else if (Modifier.isProtected(mods)) {
+			builder.append(SourceTemplates.CLASS_MODIFIER.format("protected"));
+		}
+
+		if (Modifier.isAbstract(mods)) {
+			builder.append(SourceTemplates.CLASS_MODIFIER.format("abstract"));
+		} else if (Modifier.isFinal(mods)) {
+			builder.append(SourceTemplates.CLASS_MODIFIER.format("final"));
+		}
+
+		if (Modifier.isStrict(mods)) {
+			builder.append(SourceTemplates.CLASS_MODIFIER.format("strictfp"));
+		}
+		return builder.toString();
+	}
+	
+	
+
 	private SourceBuilderUtils() {
 		// utility
 	}

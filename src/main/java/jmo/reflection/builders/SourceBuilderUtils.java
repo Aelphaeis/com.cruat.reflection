9a30dfaa47b9f8bf67a54ad4a44ddda0bdae77a0
Joseph Morain <morain@echoworx.com>
2018-06-28 11:52:06 -0400

    add build imports.

--- src/main/java/jmo/reflection/builders/SourceBuilderUtils.java
+++ src/main/java/jmo/reflection/builders/SourceBuilderUtils.java
@@ -1,16 +1,24 @@
 package jmo.reflection.builders;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import jmo.reflection.core.JClass;
 import jmo.reflection.core.SourceTemplates;
 
 final class SourceBuilderUtils {
+	public static final Package PRIMATIVE_PKG = double.class.getPackage();
+	public static final Package LANG_PKG = String.class.getPackage();
 
 	private static final String TEMP_LOC = "src/main/resources/template.txt";
+	
 	public static String getTemplate() {
 		try {
 			return new String(Files.readAllBytes(Paths.get(TEMP_LOC)));
@@ -51,6 +59,34 @@ final class SourceBuilderUtils {
 			builder.append(SourceTemplates.CLASS_MODIFIER.format("strictfp"));
 		}
 		return builder.toString();
+	}
+
+	public static String buildImports(Class<?> type) {
+		Method[] methods = type.getMethods();
+		Set<Class<?>> imports = new LinkedHashSet<>();
+		for (Method m : methods) {
+			imports.add(m.getReturnType());
+			imports.addAll(Arrays.asList(m.getParameterTypes()));
+		}
+		for (Field f : type.getFields()) {
+			imports.add(f.getType());
+		}
+		StringBuilder builder = new StringBuilder();
+		imports.remove(null);
+		for (Class<?> importClass : imports) {
+			Package pac = importClass.getPackage();
+			if (pac == null || LANG_PKG.equals(pac)) {
+				continue;
+			}
+			String importName = importClass.getName();
+			builder.append(SourceTemplates.IMPORT.format(importName));
+		}
+		builder.append('\n');
+		return builder.toString();
+	}
+	
+	public static String buildPackage(Class<?> type) {
+		return SourceTemplates.PACKAGE.format(type.getPackage().getName());
 	}
 
 	private SourceBuilderUtils() {

57210abc39443a786e2c5b4a07d8459de511b961
Joseph Morain <morain@echoworx.com>
2018-06-28 11:49:16 -0400

    constants.

--- src/main/java/jmo/reflection/builders/SourceBuilder.java
+++ src/main/java/jmo/reflection/builders/SourceBuilder.java
@@ -1,13 +1,22 @@
 package jmo.reflection.builders;
 
-import static jmo.reflection.builders.SourceBuilderUtils.getTemplate;
+import static jmo.reflection.builders.SourceBuilderUtils.*;
 
 import jmo.reflection.core.JClass;
 
 public class SourceBuilder {
 	public static final String PACKAGE = "{{package}}";
+	public static final String IMPORTS = "{{imports}}";
+	public static final String C_ANNOTATION = "{{c-annotation}}";
+	public static final String C_MODIFIERS = "{{c-modifiers}}";
+	public static final String CONSTRUCT = "{{construct}}";
+	public static final String C_NAME = "{{c-name}}";
+	public static final String S_FIELDS = "{{s-fields}}";
+	public static final String S_METHODS = "{{s-methods}}";
+	public static final String I_FIELDS = "{{i-fields}}";
+	public static final String CTORS = "{{ctors}}";
+	public static final String METHODS = "{{methods}}";
 	public static final String TEMPLATE = getTemplate();
-
 
 	final JClass<?> cls;
 

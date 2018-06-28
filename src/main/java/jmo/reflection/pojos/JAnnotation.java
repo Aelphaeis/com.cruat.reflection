99df9052aee8f1dbb70d552881432bb669b1b4b4
Joseph Morain <morain@echoworx.com>
2018-06-28 10:16:49 -0400

    created constructor for annotation.

--- src/main/java/jmo/reflection/pojos/JAnnotation.java
+++ src/main/java/jmo/reflection/pojos/JAnnotation.java
@@ -1,5 +1,12 @@
 package jmo.reflection.pojos;
 
+import java.lang.annotation.Annotation;
+
 public class JAnnotation {
 
+	Annotation annotation;
+	
+	public JAnnotation(Annotation annotation) {
+		this.annotation = annotation;
+	}
 }

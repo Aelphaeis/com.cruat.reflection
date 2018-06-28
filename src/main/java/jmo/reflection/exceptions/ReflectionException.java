3b9c33492a4f1ae0f1028113a10e4b1df7dc3c2c
Joseph Morain <morain@echoworx.com>
2018-06-28 10:13:18 -0400

    added reflection exception

---
+++ src/main/java/jmo/reflection/exceptions/ReflectionException.java
@@ -0,0 +1,22 @@
+package jmo.reflection.exceptions;
+
+public class ReflectionException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ReflectionException() {
+		super();
+	}
+
+	public ReflectionException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ReflectionException(String message) {
+		super(message);
+	}
+
+	public ReflectionException(Throwable cause) {
+		super(cause);
+	}
+}

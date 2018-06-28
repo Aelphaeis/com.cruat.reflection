0db3b802408cfe2b593543f44e991440fa2def4b
Joseph Morain <morain@echoworx.com>
2018-06-28 13:41:00 -0400

    test made for prototyping.

--- src/test/java/jmo/reflection/builders/SourceBuilderTest.java
+++ src/test/java/jmo/reflection/builders/SourceBuilderTest.java
@@ -2,8 +2,16 @@ package jmo.reflection.builders;
 
 import org.junit.Test;
 
+import jmo.reflection.core.JClass;
+import jmo.reflection.core.JClassDecorator;
+
 public class SourceBuilderTest {
 
+	@Test
+	public void test() {
+		JClass<SourceBuilder> cls = new JClassDecorator<>(SourceBuilder.class);
+		System.out.println(new SourceBuilder(cls));
+	}
 	@Test(expected = NullPointerException.class)
 	public void ctor_null_NPE() {
 		new SourceBuilder(null);
